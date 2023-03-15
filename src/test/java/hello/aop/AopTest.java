1a78cdcb29e463594fe8a2d5ec16043f0bfc7509
skahn <d1215@naver.com>
2023-03-15 21:24:27 +0900

    feat: Add test code for Aop

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,16 +2,23 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+//@Import(AspectV1.class)
+//@Import(AspectV3.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
     @Autowired
@@ -30,7 +37,6 @@ public class AopTest {
     void success() {
         orderService.orderItem("itemA");
     }
-
 
     @Test
     void exception() {
